6baca3543788a0a3bd2cc3c6788efd021486af5b
evrardco <colin.evrard@student.uclouvain.be>
2019-02-25 14:57:06 +0100

    "Centré Bouton "

--- core/src/com/fixthewall/screens/StartScreen.java
+++ core/src/com/fixthewall/screens/StartScreen.java
@@ -4,42 +4,65 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.fixthewall.game.Game;
 
 import java.util.ArrayList;
 
 public class StartScreen implements Screen {
     private Stage stage;
+    private SpriteBatch batch;
+    private Texture img;
+    private Texture imgWall;
+    private Texture imgFond;
+
     public StartScreen(final Game game){
-        //weird hack for weird errors
+
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        imgWall = new Texture("theWall.png");
+        imgFond = new Texture("fondWall.png");
+        stage = new Stage();
+
+
+
+        //Import font
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("data/Germania.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 120;
         BitmapFont font12 = generator.generateFont(parameter); // font size 12 pixels
         generator.dispose(); // don't forget to dispose to avoid memory leaks!
 
-        stage = new Stage();
+        //create button style
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
-
         font12.setColor(Color.RED);
         style.font = font12;
+
+        //setup Button
         Button playButton = new TextButton("Start", style);
-        playButton.setPosition(0,0);
+        float x = Gdx.graphics.getWidth()/2;
+        float y = Gdx.graphics.getHeight()/2;
+        x = x - playButton.getWidth()/2;
+        y = y - playButton.getHeight()/2;
+
+        playButton.setPosition(x, y);
+
         playButton.addListener(new ChangeListener() {
             @Override
             public void changed (ChangeEvent event, Actor actor) {
                 game.setScreen(new GameScreen(game));
-
             }
         });
-
+        //add button to the scene
         stage.addActor(playButton);
         //necessaire pour rendre le bouton clickable
         Gdx.input.setInputProcessor(stage);
@@ -52,9 +75,13 @@ public class StartScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
-        // Fills the screen with the selected color
+        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(imgFond, 0, 0);
+        batch.draw(imgWall, 0, 300);
+        batch.end();
+
         stage.act();
         stage.draw();
     }
@@ -82,5 +109,10 @@ public class StartScreen implements Screen {
     @Override
     public void dispose() {
         stage.dispose();
+
+        batch.dispose();
+        img.dispose();
+        imgWall.dispose();
+        imgWall.dispose();
     }
 }
