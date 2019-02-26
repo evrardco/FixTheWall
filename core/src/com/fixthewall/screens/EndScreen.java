56a076a9ef24bb7175c4ac5f38103e68047c0082
Jean-Francois <jean-francois.sambon@student.uclouvain.be>
2019-02-26 12:24:00 +0100

    Ajout ecran fin de jeu

--- core/src/com/fixthewall/screens/EndScreen.java
+++ core/src/com/fixthewall/screens/EndScreen.java
@@ -2,28 +2,32 @@ package com.fixthewall.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.fixthewall.game.Game;
+import com.fixthewall.logic.WallLogic;
+
+import static com.badlogic.gdx.Gdx.app;
 
 public class EndScreen implements Screen {
     private final Game game;
     private Stage stage;
     private SpriteBatch batch;
-    private Texture imgWall;
     private Texture imgFond;
     private BitmapFont font;
-    private int healthIncrement;
-    private int bricksIncrement;
 
 
     public EndScreen(final Game game) {
@@ -36,9 +40,34 @@ public class EndScreen implements Screen {
         //Import font
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("data/Germania.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 30;
+        parameter.size = 70;
+        BitmapFont font12 = generator.generateFont(parameter); // font size 12 pixels
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        font12.setColor(Color.RED);
+        style.font = font12;
+        //setup Button
+        Button playButton = new TextButton("Restart", style);
+        float x = Gdx.graphics.getWidth()/2f;
+        float y = Gdx.graphics.getHeight()/2f;
+        x = x - playButton.getWidth()/2;
+        y = y - playButton.getHeight()/2;
+
+        playButton.setPosition(x, y);
+
+        playButton.addListener(new ChangeListener() {
+            @Override
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) {
+                System.out.print("hahahahahahahahah");
+                WallLogic.getSingleInstance().init(100);
+                game.setScreen(new GameScreen(game));
+            }
+        });
+        //add button to the scene
+        stage.addActor(playButton);
+
         font = generator.generateFont(parameter); // font size 12 pixels
         generator.dispose(); // don't forget to dispose to avoid memory leaks!
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -47,15 +76,16 @@ public class EndScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(imgFond, 0, 0);
-        font.draw(batch, "You lost ! Want to restart ?", Gdx.graphics.getWidth()/2f,
+        font.draw(batch, "Trump lost", Gdx.graphics.getWidth()/3f,
                 (float) (Gdx.graphics.getHeight()*0.9));
         batch.end();
+        stage.act();
+        stage.draw();
     }
 
     @Override
     public void dispose () {
         batch.dispose();
-        imgWall.dispose();
         imgFond.dispose();
         font.dispose();
         stage.dispose();
